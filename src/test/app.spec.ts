833566bfdde1c4e383f7280bedbb0970d079697d
aguestuser <aguestuser@riseup.net>
2018-06-08 15:43:07 -0400

    remove `app.spec.ts`

--- src/test/app.spec.ts
+++
@@ -1,8 +0,0 @@
-import {expect} from "chai"
-import {greet} from "../app/greet"
-
-describe("our test suite", () => {
-   it("works", () => {
-       expect(greet("James")).to.equal("Hi James!")
-   })
-})
