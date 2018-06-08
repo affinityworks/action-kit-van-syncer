587501e94fe62d2276064bcad5295654c26ffe69
aguestuser <aguestuser@riseup.net>
2018-06-08 15:43:17 -0400

    streamline User.spec.ts

--- src/test/User.spec.ts
+++ src/test/User.spec.ts
@@ -1,32 +1,40 @@
+
 import {expect} from "chai"
 import {User} from "../app/entity/User"
 import {createConnection, getRepository} from "typeorm"
-import "reflect-metadata"
-import {omit} from "lodash"
+import {clone, omit} from "lodash"
 
 describe("User", () => {
-    let connection
-    let userRepo
+  let userRepo
 
-    before(async () => {
-        connection = await createConnection()
-        userRepo = getRepository(User)
-    })
+  before(async () => {
+    await createConnection()
+    userRepo = getRepository(User)
+  })
+  after( async () => await userRepo.delete({})) // delete all users
 
-    beforeEach(async () => {
-        await userRepo.create({
-            firstName: "Timber",
-            lastName: "Saw",
-            age: 25
-        })
-    })
+  it("starts with an empty repo", async () => {
+      expect(await userRepo.count()).to.equal(0)
+  })
+
+  describe("creating a user", () => {
+    // NOTE on setup
+    // - `create` doesn't save by default (:. we call `save` directly instead of `create` then `save`)
+    // - `save` borrows reference to its arg and will mutate it (:. we clone `sampleUser` before passing)
+    const sampleUser = { firstName: "Timber", lastName: "Saw", age: 25 }
+    before(async () => await userRepo.save(clone(sampleUser)))
+    after(async () => await userRepo.delete(sampleUser))
 
     it("creates a User", async () => {
-        const user = await userRepo.findOne({ firstName: "Timber" })
-        expect(omit(user, ["id"])).to.eql({
-            age: 25,
-            firstName: "Timber",
-            lastName: "Saw"
-        })
+      expect(await userRepo.count()).to.equal(1)
     })
+
+    it("creates user with correct fields", async () => {
+      expect(omit(await userRepo.findOne(), ["id"])).to.eql(sampleUser)
+    })
+
+    it("finds a user by name", async () => {
+      expect(omit(await userRepo.findOne({ firstName: "Timber" }), ["id"])).to.eql(sampleUser)
+    })
+  })
 })
