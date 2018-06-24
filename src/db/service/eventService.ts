79ba2989267054cefea99bf85d58726029976a14
aguestuser <aguestuser@riseup.net>
2018-06-24 04:04:06 -0400

    [ag] support nested attributes in `eventService#create` (yay!)

--- src/db/service/eventService.ts
+++ src/db/service/eventService.ts
@@ -1,9 +1,33 @@
 import {Database} from "../index"
 import {EventAttributes, EventInstance} from "../models/event"
-import Bluebird = require("bluebird")
 
-export const create = (db: Database, attrs: EventAttributes): Bluebird<EventInstance> =>
-  db.event.create(attrs)
+export const create = (db: Database) => async (eventTree: EventAttributes): Promise<EventInstance> => {
 
-export const createMany = (db: Database, attrs: EventAttributes[]): Bluebird<EventInstance[]> =>
-  db.event.bulkCreate(attrs)
+  // TODO: would be nice to wrap this all in a transaction
+  const event = await db.event.create(eventTree, {
+    include: [
+      { model: db.shift },
+      { model: db.location, include: [{ model: db.address }] },
+    ],
+  })
+
+  // TODO: would be nice to use `bulkCreate` here, but can't include associations in that func
+  await Promise.all(
+    (eventTree.signups || []).map(
+      signup => db.signup.create({
+        ...signup,
+        eventId: event.id,
+        shiftId: event.shifts[0].id,
+        locationId: event.locations[0].id,
+        person: signup.person,
+      }, {
+        include: [{ model: db.person, include: [{ model: db.address }]}],
+      }),
+    ),
+  )
+
+  return event
+}
+
+export const createMany = (db: Database) => (attrs: EventAttributes[]): Promise<EventInstance[]> =>
+  Promise.all(attrs.map(create(db)))
--- test/db/service/eventService.spec.ts
+++ test/db/service/eventService.spec.ts
@@ -1,28 +1,53 @@
 import {expect} from "chai"
-import {describe, it, before, after, beforeEach} from "mocha"
-import {initDb} from "../../../src/db/index"
+import {describe, it, before, after, beforeEach, afterEach} from "mocha"
+import {initDb} from "../../../src/db"
 import * as eventService from "../../../src/db/service/eventService"
-import {vanEvents} from "../../fixtures/vanEvent"
+import {vanEvents, vanEventTree} from "../../fixtures/vanEvent"
 
 describe("event service", () => {
   let db
-
-  before(() => db = initDb())
-  after(async () => await db.sequelize.close())
+  const teardown = () =>
+    Promise.all([
+      db.event.destroy({where: {}}),
+      db.shift.destroy({ where: {}}),
+      db.location.destroy({ where: {}}),
+      db.address.destroy({ where: {}}),
+      db.signup.destroy({ where: {}}),
+      db.person.destroy({ where: {}}),
+    ])
   
-  beforeEach(async () => await db.event.destroy({where: {}}))
+  before(async () => db = initDb())
+  after(async () => await db.sequelize.close())
+  beforeEach(async () => await teardown())
+  afterEach(async () => await teardown())
   
   it("creates an event", async () => {
-    await eventService.create(db, vanEvents[0])
+    await eventService.create(db)(vanEvents[0])
     expect(await db.event.count()).to.eql(1)
   })
   
   it("creates many events", async () => {
-    await eventService.createMany(db, vanEvents)
+    await eventService.createMany(db)(vanEvents)
     expect(await db.event.count()).to.eql(2)
   })
   
-  it("creates an event with associations")
+  it("creates an event with associations", async () => {
+    await eventService.create(db)(vanEventTree[0])
+    expect(await db.event.count()).to.eql(1)
+    expect(await db.shift.count()).to.eql(1)
+    expect(await db.location.count()).to.eql(1)
+    expect(await db.address.count()).to.eql(3) // 1 for event, 2 for people
+    expect(await db.signup.count()).to.eql(2)
+    expect(await db.person.count()).to.eql(2)
+  })
   
-  it("creates many events with associations")
+  it("creates many events with associations", async() => {
+    await eventService.createMany(db)(vanEventTree)
+    expect(await db.event.count()).to.eql(2)
+    expect(await db.shift.count()).to.eql(2)
+    expect(await db.location.count()).to.eql(2)
+    expect(await db.address.count()).to.eql(4) // 1 for event, 2 for people
+    expect(await db.signup.count()).to.eql(2)
+    expect(await db.person.count()).to.eql(2)
+  })
 })
