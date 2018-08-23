37a10640cb2b7783fdf4e5301e65dcd0911e7fd3
James Vaughan <vaughanj10@gmail.com>
2018-08-23 08:25:07 +0200

    Add repeatSync to index.

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,25 @@ import {getEventTrees} from "./service/actionKitAPI"
 import {parseVanEvents} from "../src/service/parse"
 import {initDb} from "../src/db"
 import * as eventService from "../src/db/service/eventService"
+import {repeatEvery} from "../test/support/time"
 
 export const sync = async (db = initDb()) => {
+  console.log("[SYNC STARTED] ", Date.now())
   const eventTrees = await getEventTrees()
   const vanEventTrees = parseVanEvents(eventTrees)
   await eventService.saveMany(db)(vanEventTrees)
 }
 
+const SECOND = 1000
+const MINUTE = 60 * SECOND
+const HOUR = 60 * MINUTE
+const THIRTY_SECONDS = 30 * SECOND
+const EIGHT_HOURS = 8 * HOUR
+
+export const repeatSync = async () => {
+  repeatEvery(THIRTY_SECONDS, await sync) // TODO: Change to EIGHT_HOURS for prod
+}
+
 // (async () => {
-//   sync()
+//   repeatSync()
 // })()
